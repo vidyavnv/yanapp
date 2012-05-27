539eaa40431a5d5b45e65b503910e4842767c00a
Vidya <vidya.vnv@gmail.com>
2012-05-27 16:45:09 +0530

    Add all files

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-<HTML>
-<BODY>
-Welcome User!
-</BODY>
-</HTML>
+<html>
+<body>
+<?php
+$fp=fopen("store/counter.txt","r");
+$text=fgets($fp);
+fclose($fp);
+$counter=(int)$text+1;
+$fp=fopen("store/counter.txt","w");
+fwrite($fp,$counter."\n");
+fclose($fp);
+echo 'Hello! You are visitor number&nbsp;'.$counter; 
+?>
+</body>
+</html>
